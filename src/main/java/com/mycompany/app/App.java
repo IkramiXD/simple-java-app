4f9e1af31528cdb05520bd4cd84658109fa7ee0b
Ikrami <ikramiaswan@gmail.com>
2025-04-09 16:17:55 +0800

    My 2nd change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Modofied from local project!");
+        System.out.println("Modified from local project v2!");
     }
 
     public String getMessage() {
