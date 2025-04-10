1937ee366560bcdcf6edc7a43b08cc3a8022e1c4
Ikrami <ikramiaswan@gmail.com>
2025-04-10 14:52:59 +0800

    fix error

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Modified from local project!");
+        System.out.println("Modified from local project! v1");
     }
 
     public String getMessage() {
